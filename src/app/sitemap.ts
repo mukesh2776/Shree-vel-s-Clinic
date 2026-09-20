1fad273fcd5ddb91e75d45db7953bc6236435783
mukesh2776 <mukeshsr943@gmail.com>
2026-09-20 11:46:22 +0530

    Update sitemap base URL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.shreevelsclinic.com';
+  const baseUrl = 'https://www.shreevels.online/';
 
   const routes = [
     '',
